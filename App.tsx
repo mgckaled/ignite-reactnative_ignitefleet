89ee19ed3acad64dedb161aadef1af399a94a0a4
mgckaled <marcelgckaled@gmail.com>
2023-05-06 10:53:26 -0300

    feat: create Loading component and add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto"
 import { ThemeProvider } from "styled-components/native"
 
 import theme from "./src/theme"
 
+import { StatusBar } from "react-native"
+import { Loading } from "./src/components/Loading"
 import { SignIn } from "./src/screens/SignIn"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
       <SignIn />
     </ThemeProvider>
   )
