bb4276bb4d9d836c209e23c87a30c319dcfdbe2e
mgckaled <marcelgckaled@gmail.com>
2023-05-07 19:19:08 -0300

    feat: run expo prebuild

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+
+const { getDefaultConfig } = require("expo/metro-config")
+
+module.exports = getDefaultConfig(__dirname)
